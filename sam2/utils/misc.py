8a99207d1ca1938fbfbc9966334a253d62e78c72
Hunor Laczko <luni741@gmail.com>
2025-07-18 10:11:18 +0000

    add png support

--- sam2/utils/misc.py
+++ sam2/utils/misc.py
@@ -244,7 +244,7 @@ def load_video_frames_from_jpg_images(
     frame_names = [
         p
         for p in os.listdir(jpg_folder)
-        if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG", ".avif", ".AVIF"]
+        if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG", ".avif", ".AVIF", ".png", ".PNG"]
     ]
     # frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
     frame_names.sort()
